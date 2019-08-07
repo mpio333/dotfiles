825dfcf12e1d46c20bd3c2cdee5aa2063ce4d948
fzic <sistemastv.dm@gmail.com>
2019-08-07 16:14:13 -0300

    moved script to .lcoal/bin

---
+++ .local/bin/autorun.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+setxkbmap -layout us -variant altgr-intl -option caps:escape
+
+feh --bg-fill ~/.config/wallpaper
+run redshift
+bash ~/.local/bin/trackpad_enable_tap
+run thunar --daemon
+run compton -b
+# run cmst
+run mpd
